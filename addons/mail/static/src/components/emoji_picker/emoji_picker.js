93975e7c2e1c83e0301efa51c263be09c1b60ef4
Nemael <mapa@odoo.com>
2022-06-27 12:52:14 +0200

    [REM] mail: remove dead code from EmojiPicker
    This commit removes dead code from EmojiPicker
    
    Part of Task-2365687

--- addons/mail/static/src/components/emoji_picker/emoji_picker.js
+++ addons/mail/static/src/components/emoji_picker/emoji_picker.js
@@ -1,53 +1,10 @@
 /** @odoo-module **/
 
-import { useUpdate } from '@mail/component_hooks/use_update';
 import { registerMessagingComponent } from '@mail/utils/messaging_component';
-import { LegacyComponent } from '@web/legacy/legacy_component';
 
-export class EmojiPicker extends LegacyComponent {
+const { Component } = owl;
 
-    /**
-     * @override
-     */
-    setup() {
-        super.setup();        
-        useUpdate({ func: () => this._update() });
-    }
-
-    get emojiPickerView() {
-        return this.props.record;
-    }
-
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-
-    /**
-     * @private
-     */
-    _update() {
-        this.trigger('o-popover-compute');
-    }
-
-    //--------------------------------------------------------------------------
-    // Public
-    //--------------------------------------------------------------------------
-
-    close() {
-        this.trigger('o-popover-close');
-    }
-
-    /**
-     * Returns whether the given node is self or a children of self.
-     *
-     * @param {Node} node
-     * @returns {boolean}
-     */
-    contains(node) {
-        return Boolean(this.root.el && this.root.el.contains(node));
-    }
-
-}
+export class EmojiPicker extends Component {}
 
 Object.assign(EmojiPicker, {
     props: { record: Object },
