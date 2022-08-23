56d5f50d7273f364b0e4933789dc5d2d8e875af8
Benoit Socias <bso@odoo.com>
2022-08-23 15:14:13 +0200

    [REV] website: searchbar: wrong usage of BS dropdown
    This reverts commit 358194e99ada7d91e1295392d96d2f4277f35378.
    
    That commit did break the search across website:
    - search was not triggered by pressing ENTER anymore
    - search was not triggered by clicking on search icon anymore
    - search reset button had no effect (cross icon)
    - displaying suggestions, focusing out then selecting the search
    field again did trigger an error popup
    
    task-2960518

--- addons/website/static/src/snippets/s_searchbar/000.js
+++ addons/website/static/src/snippets/s_searchbar/000.js
@@ -33,7 +33,6 @@ publicWidget.registry.searchBar = publicWidget.Widget.extend({
      */
     start: function () {
         this.$input = this.$('.search-query');
-        this.$searchGroup = this.$('.input-group');
 
         this.searchType = this.$input.data('searchType');
         this.order = this.$('.o_search_order_by').val();
@@ -150,6 +149,7 @@ publicWidget.registry.searchBar = publicWidget.Widget.extend({
         }
 
         const $prevMenu = this.$menu;
+        this.$el.toggleClass('dropdown show', !!res);
         if (res && this.limit) {
             const results = res['results'];
             let template = 'website.s_searchbar.autocomplete';
@@ -166,7 +166,6 @@ publicWidget.registry.searchBar = publicWidget.Widget.extend({
                 widget: this,
             }));
             this.$menu.css('min-width', this.autocompleteMinWidth);
-            this.$searchGroup[0].dataset['bsToggle'] = 'dropdown';
 
             // Handle the case where the searchbar is in a mega menu by making
             // it position:fixed and forcing its size. Note: this could be the
@@ -202,7 +201,6 @@ publicWidget.registry.searchBar = publicWidget.Widget.extend({
             });
         }
 
-        this.$el.toggleClass('dropdown show', !!res);
         if ($prevMenu) {
             $prevMenu.remove();
         }
