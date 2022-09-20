7af97e2e788780aa07e4079b6897b0657a758262
Adesh Jolhe (adjo) <adjo@odoo.com>
2022-09-20 23:38:26 +0530

    [IMP] sale_(project,hr)_timesheet: bring generic improvements for project
    The purpose of this PR is to improve the generic usage of the project app.
    
    So in this PR done following changes:
     - In project.task form view added the name of the parent task on stat button.
       Transformed milestone form view quantity field
     - switched Sales Orders button and planning button from their place in
       the project kanban card burger menu
     - In milestone form view added decoration-danger on quantity % if it is equal
       to 0 and there is an SOL set
     - On milestone creation the SOL of the project set default if it is of
       milestone type
     - In the product form view Uom is set as 'hours' if the service is either
       prepaid or invoiced based on timesheets.
     - In project.sharing improved remaining hours widget now only the frame is
       displayed in orange/red, not the background (as in the back-end)
     - project.update right-side panel added the ref of the SO in the nameget
       of the SOL
     - In project.update right-side panel added Confirmation when trying to delete
       a milestone
     - Reduced the size of project and task sharing wizard
    
    task-2989828

--- addons/sale_project/models/project_milestone.py
+++ addons/sale_project/models/project_milestone.py
@@ -7,14 +7,47 @@ class ProjectMilestone(models.Model):
     _name = 'project.milestone'
     _inherit = 'project.milestone'
 
+    def _default_sale_line_id(self):
+        project_id = self._context.get('default_project_id')
+        if project_id:
+            return self.env['project.project'].browse(project_id).sale_line_id.filtered(lambda sol: sol.qty_delivered_method == 'milestones')
+
     allow_billable = fields.Boolean(related='project_id.allow_billable')
     project_partner_id = fields.Many2one(related='project_id.partner_id')
 
-    sale_line_id = fields.Many2one('sale.order.line', 'Sales Order Item', help='Sales Order Item that will be updated once the milestone is reached.',
+    sale_line_id = fields.Many2one('sale.order.line', 'Sales Order Item', default=_default_sale_line_id, help='Sales Order Item that will be updated once the milestone is reached.',
         domain="[('order_partner_id', '=?', project_partner_id), ('qty_delivered_method', '=', 'milestones')]")
-    quantity_percentage = fields.Float('Quantity', help='Percentage of the ordered quantity that will automatically be delivered once the milestone is reached.')
+    sale_line_name = fields.Text(compute="_compute_sale_line_id_name")
+    quantity_percentage = fields.Float('Quantity Percentage', compute="_compute_quantity_percentage", store=True, help='Percentage of the ordered quantity that will automatically be delivered once the milestone is reached.')
+    product_uom_qty = fields.Float(related="sale_line_id.product_uom_qty")
+    product_uom = fields.Many2one(related="sale_line_id.product_uom")
+    total_qty = fields.Float("Total Quantity", compute="_compute_total_qty", inverse="_inverse_total_qty")
+    remaining_percentage = fields.Float("Remaining Percentage", compute="_compute_remaining_quantity")
 
-    sale_line_name = fields.Text(related='sale_line_id.name')
+    @api.depends('sale_line_id.name')
+    def _compute_sale_line_id_name(self):
+        for rec in self:
+            rec.sale_line_name = rec.sale_line_id.display_name
+
+    @api.depends('product_uom_qty', 'total_qty')
+    def _compute_quantity_percentage(self):
+        for milestone in self:
+            milestone.quantity_percentage = milestone.product_uom_qty and milestone.total_qty / milestone.product_uom_qty
+
+    @api.depends('quantity_percentage', 'product_uom_qty')
+    def _compute_total_qty(self):
+        for milestone in self:
+            milestone.total_qty = milestone.quantity_percentage * milestone.product_uom_qty
+
+    @api.onchange('product_uom_qty', 'total_qty')
+    def _inverse_total_qty(self):
+        for milestone in self:
+            milestone.quantity_percentage = milestone.product_uom_qty and milestone.total_qty / milestone.product_uom_qty
+
+    @api.depends('quantity_percentage', 'total_qty')
+    def _compute_remaining_quantity(self):
+        for milestone in self:
+            milestone.remaining_percentage = milestone.product_uom_qty and (milestone.product_uom_qty - milestone.total_qty) / milestone.product_uom_qty
 
     @api.model
     def _get_fields_to_export(self):
