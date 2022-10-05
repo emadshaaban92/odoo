2b20b9489b64020c2e9635273aa20d6ca8312a1f
Claire Bretton (clbr) <clbr@odoo.com>
2022-10-05 10:01:21 +0200

    [IMP] l10n_ie: Rework of Ireland localization
    Adds or improves for Republic of Ireland localization:
    - our custom and more detailed Chart of Accounts,
    - up to date Taxes (as of end of 2022),
    - fiscal positions.

--- addons/l10n_ie/__manifest__.py
+++ addons/l10n_ie/__manifest__.py
@@ -1,28 +1,29 @@
-# -*- encoding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
-
 {
-    'name': 'Ireland - Accounting',
-    'version': '1.0',
-    'category': 'Accounting/Localizations/Account Charts',
-    'description': """
-    This module is for all the Irish SMEs who would like to setup their accounting quickly. The module provides:
-
-    - a Chart of Accounts customised to Ireland
-    - VAT Rates and Structure""",
-
-    'author': 'Target Integration',
-    'website': 'http://www.targetintegration.com',
-    'depends': ['account', 'base_iban', 'base_vat'],
-    'data': [
-        'data/account_chart_template.xml',
-        'data/account.account.template.csv',
-        'data/account.chart.template.csv',
-        'data/account_tax_data.xml',
-        'data/account_chart_template_configuration_data.xml',
+    "name": "Ireland - Accounting",
+    "version": "1.0",
+    "category": "Accounting/Localizations/Account Charts",
+    "description": """
+    This is the base module to manage the accounting for Republic of Ireland in Odoo. 
+    """,
+    "author": "Odoo SA",
+    "depends": [
+        "account",
+        "base_vat",
     ],
-    'demo': [
-        'demo/demo_company.xml',
+    "data": [
+        "data/account_chart_template_data.xml",
+        "data/account.account.template.csv",
+        "data/account.group.template.csv",
+        "data/l10n_ie_chart_data.xml",
+        "data/tax_report.xml",
+        "data/account_tax_group_data.xml",
+        "data/account_tax_template_data.xml",
+        "data/account_fiscal_position_template.xml",
+        "data/account_chart_template_try_loading.xml",
     ],
-    'license': 'LGPL-3',
+    "demo": [
+        "demo/demo_company.xml",
+    ],
+    "license": "LGPL-3",
 }
