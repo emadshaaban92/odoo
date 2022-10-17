ee2ab0f040a5b29156b432a2ac8207824cc79a45
Kevin Baptiste <kba@odoo.com>
2022-10-17 12:10:38 +0200

    [IMP] product: enable Imperial units at install when required
    The imperial units are now automatically enabled at the module
    installation if *all* the companies use imperial units (USA, Myanmar, and
    Liberia).
    
    task-2951823

--- addons/product/__init__.py
+++ addons/product/__init__.py
@@ -5,3 +5,15 @@ from . import models
 from . import report
 from . import populate
 from . import wizard
+
+from odoo import api, SUPERUSER_ID
+
+def _post_init_hook(cr, registry):
+    env = api.Environment(cr, SUPERUSER_ID, {})
+
+    companies = env['res.company'].search([])
+    country_codes = ['US', 'MM', 'LR']
+
+    if all(c.country_id.code in country_codes for c in companies):
+        env['ir.config_parameter'].sudo().set_param('product.weight_in_lbs', '1')
+        env['ir.config_parameter'].sudo().set_param('product.volume_in_cubic_feet', '1')
