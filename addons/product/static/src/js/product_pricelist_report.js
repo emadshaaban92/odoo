9cd6f770ec5fb0bc970324527fbbeb402a54d732
Horacio Tellez <hote@odoo.com>
2022-11-08 09:55:13 +0100

    [IMP] product: migrate `product_pricelist_report` to OWL
    Migration of the js code of `product_pricelist_report` to OWL.

--- addons/product/static/src/js/product_pricelist_report.js
+++ addons/product/static/src/js/product_pricelist_report.js
@@ -1,317 +1,241 @@
-odoo.define('product.generate_pricelist', function (require) {
-'use strict';
+/** @odoo-module **/
 
-var AbstractAction = require('web.AbstractAction');
-var core = require('web.core');
-var FieldMany2One = require('web.relational_fields').FieldMany2One;
-var StandaloneFieldManagerMixin = require('web.StandaloneFieldManagerMixin');
-var Widget = require('web.Widget');
+import { registry } from "@web/core/registry";
+import { sprintf } from "@web/core/utils/strings";
+import { useService } from "@web/core/utils/hooks";
+import { useSetupAction } from "@web/webclient/actions/action_hook";
+import { Layout } from "@web/search/layout";
 
-var QWeb = core.qweb;
-var _t = core._t;
+const { Component, useState, onWillStart, markup, onRendered } = owl;
 
-var QtyTagWidget = Widget.extend({
-    template: 'product.report_pricelist_qty',
-    events: {
-        'click .o_remove_qty': '_onClickRemoveQty',
-    },
-    /**
-     * @override
-     */
-    init: function (parent, defaulQuantities) {
-        this._super.apply(this, arguments);
-        this.quantities = defaulQuantities;
+
+export class ProductPricelistReport extends Component {
+    setup() {
+        this.action = useService("action");
+        this.orm = useService("orm");
+
         this.MAX_QTY = 5;
-    },
+        const pastState = this.props.state || {};
 
-    //--------------------------------------------------------------------------
-    // Handlers
-    //--------------------------------------------------------------------------
+        // Putting everything that needs to be reactive on the useState wrapper.
 
-    /**
-     * Add a quantity when add(+) button clicked.
-     *
-     * @private
-     */
-    _onClickAddQty: function () {
+        // While activeIds and activeModel are not necessarily reactive we need
+        // them to be keeped for the useSetupAction going back and forward on links.
+        this.state = useState({
+            activeIds: pastState.active_ids || this.props.action.context.active_ids,
+            activeModel: pastState.active_model || this.props.action.context.active_model,
+            displayPricelist: pastState.displayPricelist || false,
+            html: markup(""),
+            pricelists: [],
+            quantities: pastState.quantities || [1, 5, 10],
+            selectedPricelist: {},
+        });
+
+        onWillStart(async () => {
+            this.state.pricelists = await this.getPricelists()
+            this.state.selectedPricelist = pastState.selectedPricelist || this.state.pricelists[0];
+
+            this.renderHtml();
+        });
+
+        onRendered(() => {
+            this.env.config.setDisplayName(this.displayName);
+        });
+
+        /*
+        When following the link of a product and coming back we need to keep the 
+        precedent state:
+            - if the pricelist was being showed
+            - wich pricelist is selected at the moment
+            - which quantities
+            - the products (or product variants) for which the report was opened
+            - if the active model is product.product or product.template
+        */
+        useSetupAction({
+            getLocalState: () => {
+                return {
+                    active_ids: this.active_ids,
+                    active_model: this.active_model,
+                    displayPricelist: this.displayPricelist,
+                    quantities: this.quantities,
+                    selectedPricelist: this.selectedPricelist,
+              };
+            },
+        });
+    }
+
+    get activeIds() {
+        return this.state.activeIds;
+    }
+
+    get activeModel() {
+        return this.state.activeModel;
+    }
+
+    get displayName() {
+        return this.env._t("Pricelist Report");
+    }
+
+    get displayPricelist() {
+        return this.state.displayPricelist;
+    }
+
+    get getReportParams() {
+        return {
+            active_model: this.activeModel || 'product.template',
+            active_ids: this.state.activeIds || [],
+            is_visible_title: this.displayPricelist || '',
+            pricelist_id: this.selectedPricelist.id || '',
+            quantities: this.quantities || [1],
+        };
+    }
+
+    get html() {
+        return this.state.html;
+    }
+
+    get pricelists() {
+        return this.state.pricelists;
+    }
+
+    get quantities() {
+        return this.state.quantities;
+    }
+    get selectedPricelist() {
+        return this.state.selectedPricelist;
+    }
+
+    // orm calls
+
+    getPricelists() {
+        return this.orm.searchRead("product.pricelist", [], ["id", "name"]);
+    }
+
+    async renderHtml() {
+        let html = await this.orm.call(
+            "report.product.report_pricelist", "get_html", [], {data: this.getReportParams}
+        );
+        this.state.html = markup(html);
+    }
+
+    // events
+
+    async onClickAddQty(ev) {
         if (this.quantities.length >= this.MAX_QTY) {
-            this.displayNotification({ message: _.str.sprintf(
-                _t("At most %d quantities can be displayed simultaneously. Remove a selected quantity to add others."),
+            let message = sprintf(
+                this.env._t("At most %s quantities can be displayed simultaneously. Remove a selected quantity to add others."),
                 this.MAX_QTY
-            ) });
+            );
+            await this.action.doAction({
+                type: "ir.actions.client",
+                tag: "display_notification",
+                params: {
+                    "type": "warning",
+                    "message": message,
+                },
+            });
             return;
         }
-        const qty = parseInt(this.$('.o_product_qty').val());
+
+        const qty = parseInt($("input.add-quantity-input")[0].value);
         if (qty && qty > 0) {
-            // Check qty already exist
+            // Check qty already exist.
             if (this.quantities.indexOf(qty) === -1) {
-                this.quantities.push(qty);
-                this.quantities = this.quantities.sort((a, b) => a - b);
-                this.trigger_up('qty_changed', {quantities: this.quantities});
-                this.renderElement();
+                this.state.quantities.push(qty);
+                this.state.quantities = this.state.quantities.sort((a, b) => a - b);
+                this.renderHtml();
             } else {
-                this.displayNotification({
-                    message: _.str.sprintf(_t("Quantity already present (%d)."), qty),
-                    type: 'info'
+                let message = this.env._t("Quantity already present");
+                await this.action.doAction({
+                    type: "ir.actions.client",
+                    tag: "display_notification",
+                    params: {
+                        "type": "info",
+                        "message": `${message} (${qty}).`,
+                    },
                 });
             }
         } else {
-            this.displayNotification({ message: _t("Please enter a positive whole number") });
-        }
-    },
-    /**
-     * Remove quantity.
-     *
-     * @private
-     * @param {jQueryEvent} ev
-     */
-    _onClickRemoveQty: function (ev) {
-        const qty = parseInt($(ev.currentTarget).closest('.badge').data('qty'));
-        this.quantities = this.quantities.filter(q => q !== qty);
-        this.trigger_up('qty_changed', {quantities: this.quantities});
-        this.renderElement();
-    },
-});
-
-var GeneratePriceList = AbstractAction.extend(StandaloneFieldManagerMixin, {
-    hasControlPanel: true,
-    events: {
-        'click .o_action': '_onClickAction',
-        'submit form': '_onSubmitForm',
-    },
-    custom_events: Object.assign({}, StandaloneFieldManagerMixin.custom_events, {
-        field_changed: '_onFieldChanged',
-        qty_changed: '_onQtyChanged',
-    }),
-    /**
-     * @override
-     */
-    init: function (parent, params) {
-        this._super.apply(this, arguments);
-        StandaloneFieldManagerMixin.init.call(this);
-        this.context = params.context;
-        // in case the window got refreshed
-        if (params.params && params.params.active_ids && typeof(params.params.active_ids === 'string')) {
-            try {
-                this.context.active_ids = params.params.active_ids.split(',').map(id => parseInt(id));
-                this.context.active_model = params.params.active_model;
-            } catch {
-                console.log('unable to load ids from the url fragment 🙁');
-            }
-        }
-        if (!this.context.active_model) {
-            // started without an active module, assume product templates
-            this.context.active_model = 'product.template';
-        }
-        this.context.quantities = [1, 5, 10];
-    },
-    /**
-     * @override
-     */
-    willStart: function () {
-        let getPricelist;
-        // started without a selected pricelist in context? just get the first one
-        if (this.context.default_pricelist) {
-            getPricelist = Promise.resolve([this.context.default_pricelist]);
-        } else {
-            getPricelist = this._rpc({
-                model: 'product.pricelist',
-                method: 'search',
-                args: [[]],
-                kwargs: {limit: 1}
-            });
-        }
-        const fieldSetup = getPricelist.then(pricelistIds => {
-            return this.model.makeRecord('report.product.report_pricelist', [{
-                name: 'pricelist_id',
-                type: 'many2one',
-                relation: 'product.pricelist',
-                value: pricelistIds[0],
-            }]);
-        }).then(recordID => {
-            const record = this.model.get(recordID);
-            this.many2one = new FieldMany2One(this, 'pricelist_id', record, {
-                mode: 'edit',
-                attrs: {
-                    can_create: false,
-                    can_write: false,
-                    options: {no_open: true},
+            await this.action.doAction({
+                type: "ir.actions.client",
+                tag: "display_notification",
+                params: {
+                    "type": "info",
+                    "message": this.env._t("Please enter a positive whole number."),
                 },
             });
-            this._registerWidget(recordID, 'pricelist_id', this.many2one);
-        });
-        return Promise.all([fieldSetup, this._getHtml(), this._super()]);
-    },
-    /**
-     * @override
-     */
-    start: function () {
-        this.controlPanelProps.cp_content = this._renderComponent();
-        const $content = this.controlPanelProps.cp_content;
-        $content["$searchview"][0].querySelector('.o_is_visible_title').addEventListener('click', this._onClickVisibleTitle.bind(this));
-        return this._super.apply(this, arguments).then(() => {
-            this.$('.o_content').html(this.reportHtml);
-        });
-    },
-    /**
-     * Include the current model (template/variant) in the state to allow refreshing without losing
-     * the proper context.
-     * @override
-     */
-    getState: function () {
-        return {
-            active_model: this.context.active_model,
-        };
-    },
-    getTitle: function () {
-        return _t('Pricelist Report');
-    },
+        }
+    }
 
-    //--------------------------------------------------------------------------
-    // Private
-    //--------------------------------------------------------------------------
-
-    /**
-     * Returns the expected data for the report rendering call (html or pdf)
-     *
-     * @private
-     * @returns {Object}
-     */
-    _prepareActionReportParams: function () {
-        return {
-            active_model: this.context.active_model,
-            active_ids: this.context.active_ids || '',
-            is_visible_title: this.context.is_visible_title || '',
-            pricelist_id: this.context.pricelist_id || '',
-            quantities: this.context.quantities || [1],
-        };
-    },
-    /**
-     * Get template to display report.
-     *
-     * @private
-     * @returns {Promise}
-     */
-    _getHtml: function () {
-        return this._rpc({
-            model: 'report.product.report_pricelist',
-            method: 'get_html',
-            kwargs: {
-                data: this._prepareActionReportParams(),
-                context: this.context,
-            },
-        }).then(result => {
-            this.reportHtml = result;
-        });
-    },
-    /**
-     * Reload report.
-     *
-     * @private
-     * @returns {Promise}
-     */
-    _reload: function () {
-        return this._getHtml().then(() => {
-            this.$('.o_content').html(this.reportHtml);
-        });
-    },
-    /**
-     * Render search view and print button.
-     *
-     * @private
-     */
-    _renderComponent: function () {
-        const $buttons = $('<button>', {
-            class: 'btn btn-primary',
-            text: _t("Print"),
-        }).on('click', this._onClickPrint.bind(this));
-
-        const $searchview = $(QWeb.render('product.report_pricelist_search'));
-        this.many2one.appendTo($searchview.find('.o_pricelist'));
-
-        this.qtyTagWidget = new QtyTagWidget(this, this.context.quantities);
-        this.qtyTagWidget.replace($searchview.find('.o_product_qty'));
-        return { $buttons, $searchview };
-    },
-
-    //--------------------------------------------------------------------------
-    // Handlers
-    //--------------------------------------------------------------------------
-
-    /**
-     * Checkbox is checked, the report title will show.
-     *
-     * @private
-     * @param {Event} ev
-     */
-    _onClickVisibleTitle(ev) {
-        this.context.is_visible_title = ev.currentTarget.checked;
-        this._reload();
-    },
-
-    /**
-     * Open form view of particular record when link clicked.
-     *
-     * @private
-     * @param {jQueryEvent} ev
-     */
-    _onClickAction: function (ev) {
+    onClickLink(ev) {
         ev.preventDefault();
-        this.do_action({
-            type: 'ir.actions.act_window',
-            res_model: $(ev.currentTarget).data('model'),
-            res_id: $(ev.currentTarget).data('res-id'),
-            views: [[false, 'form']],
-            target: 'self',
-        });
-    },
-    /**
-     * Print report in PDF when button clicked.
-     *
-     * @private
-     */
-    _onClickPrint: function () {
-        return this.do_action({
+
+        let classes = ev.target.getAttribute("class", "");
+        let resModel = ev.target.getAttribute("data-model", "");
+        let resId = ev.target.getAttribute("data-res-id", "");
+
+        if (classes && classes.includes("o_action") && resModel && resId) {
+            this.action.doAction({
+                type: 'ir.actions.act_window',
+                res_model: resModel,
+                res_id: parseInt(resId),
+                views: [[false, 'form']],
+                target: 'self',
+            });
+        }
+    }
+
+    onClickPrint() {
+        this.action.doAction({
             type: 'ir.actions.report',
             report_type: 'qweb-pdf',
             report_name: 'product.report_pricelist',
             report_file: 'product.report_pricelist',
-            data: this._prepareActionReportParams(),
+            data: this.getReportParams,
         });
-    },
-    /**
-     * Reload report when pricelist changed.
-     *
-     * @override
-     */
-    _onFieldChanged: function (event) {
-        this.context.pricelist_id = event.data.changes.pricelist_id.id;
-        StandaloneFieldManagerMixin._onFieldChanged.apply(this, arguments);
-        this._reload();
-    },
-    /**
-     * Reload report when quantities changed.
-     *
-     * @private
-     * @param {OdooEvent} ev
-     * @param {integer[]} event.data.quantities
-     */
-    _onQtyChanged: function (ev) {
-        this.context.quantities = ev.data.quantities;
-        this._reload();
-    },
-    _onSubmitForm: function (ev) {
-        ev.preventDefault();
-        ev.stopPropagation();
-        this.qtyTagWidget._onClickAddQty();
-    },
-});
+    }
 
-core.action_registry.add('generate_pricelist', GeneratePriceList);
+    async onClickRemoveQty(ev) {
+        if (this.quantities.length <= 1) {
+            await this.action.doAction({
+                type: "ir.actions.client",
+                tag: "display_notification",
+                params: {
+                    "type": "warning",
+                    "message": this.env._t(
+                        "You must leave at least one quantity."
+                    ),
+                },
+            });
+            return
+        }
 
-return {
-    GeneratePriceList,
-    QtyTagWidget
-};
+        const qty = parseInt(ev.srcElement.parentElement.childNodes[0].data);
+        this.state.quantities = this.state.quantities.filter(q => q !== qty);
+        this.renderHtml();
+    }
 
-});
+    onSelectPricelist(ev) {
+        this.state.selectedPricelist = this.pricelists.filter(pricelist => 
+            pricelist.id === parseInt(ev.target.value)
+        )[0];
+
+        this.renderHtml();
+    }
+
+    onToggleDisplayPricelist() {
+        this.state.displayPricelist = !this.displayPricelist;
+        this.renderHtml();
+    }
+}
+
+ProductPricelistReport.props = {
+    action: { type: Object },
+    actionId: { type: Number, optional: true },
+    className: { type: String },
+    state: { type: Object, optional: true },
+    globalState: { type: Object, optional: true },
+}
+ProductPricelistReport.components = { Layout };
+ProductPricelistReport.template = "product.ProductPricelistReport";
+registry.category("actions").add("generate_pricelist_report", ProductPricelistReport);
