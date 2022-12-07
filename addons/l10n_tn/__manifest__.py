e95bb19cb7f47321bddeb117ca4a6becaa73aabe
hupo-odoo <hupo@odoo.com>
2022-12-07 10:28:13 +0100

    [ADD] l10n_tn : added CoA and related files
    Added the first package for the Tunisian accounting localization. The chart of accounts has been added as well as all related files. Work on taxes and tax report still needs improvements. A demo company has also been added. The translation files still need to be created.
    
    task-3093908

---
+++ addons/l10n_tn/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*-
+
+{
+    "name": "Tunisia - Accounting",
+    'icon': '/l10n_tn/static/description/icon.svg',
+    "version": "1.0",
+    "author": 'Odoo S.A.',
+    "license": "LGPL-3",
+    "category": 'Accounting/Localizations/Account Charts',
+    "description": """
+This is the module to manage the accounting chart for Tunisia in Odoo.
+=======================================================================
+
+""",
+    "depends": ['base_vat', 'account'],
+    "data": [
+        'data/l10n_tn_chart_data.xml',
+        'data/account.account.template.csv',
+        'data/account.group.template.csv',
+        'data/account_chart_template_data.xml',
+        'data/account_chart_template_configure_data.xml',
+    ],
+    'demo': [
+        'demo/demo_company.xml',
+    ],
+}
