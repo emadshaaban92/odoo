32d14d5fac358841038f1a55e5fcbd994bffee46
vlst <vlst@odoo.com>
2022-12-08 17:25:36 +0100

    separated xml and js files in landingPage comp

--- addons/point_of_sale/static/src/self_order/SelfOrderRoot.js
+++ addons/point_of_sale/static/src/self_order/SelfOrderRoot.js
@@ -3,17 +3,8 @@
 const { Component, mount, xml, onRendered, whenReady, App} = owl;
 import { makeEnv, startServices } from "@web/env";
 import { setLoadXmlDefaultApp, loadJS, templates } from '@web/core/assets';
-import { MainComponentsContainer } from "@web/core/main_components_container";
 import { _t } from "@web/core/l10n/translation";
 import { LandingPage } from "./LandingPage/LandingPage.js";
-// class LandingPage extends Component {
-//     setup() {
-//         onRendered(() => {
-//                 console.log('Rendered:', this.constructor.name);
-//         });
-//     }
-//   static template = xml`<h1>Hello Owl landing page</h1>`;
-// }
 class SelfOrderRoot extends Component {
     setup() {
         onRendered(() => {
