35ae3f820fbef866457315874c0bf155f8eaf882
Joseph Caburnay <jcb@odoo.com>
2022-12-08 09:56:37 +0100

    remove dummies

--- addons/web/__manifest__.py
+++ addons/web/__manifest__.py
@@ -23,7 +23,6 @@ This module provides the core of the Odoo Web Client.
         'views/neutralize_views.xml',
         'data/ir_attachment.xml',
         'data/report_layout.xml',
-        'views/tour_manager_actions.xml',
     ],
     'assets': {
         # ---------------------------------------------------------------------
@@ -174,7 +173,6 @@ This module provides the core of the Odoo Web Client.
             'web/static/src/legacy/xml/colorpicker.xml',
             'web/static/src/legacy/js/widgets/translation_dialog.js',
             'web/static/src/legacy/xml/translation_dialog.xml',
-            'web/static/src/tour_manager/**',
         ],
         'web.assets_backend': [
             ('include', 'web._assets_helpers'),
