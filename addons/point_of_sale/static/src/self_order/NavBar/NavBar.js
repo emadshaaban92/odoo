743752c361236abee694bddc4f27cc13a9124efc
vlst <vlst@odoo.com>
2022-12-11 20:10:28 +0100

    started creating ProductList screen

--- addons/point_of_sale/static/src/self_order/NavBar/NavBar.js
+++ addons/point_of_sale/static/src/self_order/NavBar/NavBar.js
@@ -1,15 +1,11 @@
 /** @odoo-module */
 
-const { Component, mount, xml, onRendered, whenReady, App} = owl;
+const { Component } = owl;
 import { _t } from "@web/core/l10n/translation";
 
 export class NavBar extends Component {
     setup() {
-        onRendered(() => {
-                console.log('Rendered:', this.constructor.name);
-        });
     }
-//   static components = { LandingPageOutline };  
 }
 NavBar.template = 'NavBar'
 export default { NavBar };
