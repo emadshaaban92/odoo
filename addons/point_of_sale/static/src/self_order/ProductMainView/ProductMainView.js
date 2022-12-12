7b329dcb3773c7a87db6776133c660be95e85943
vlst <vlst@odoo.com>
2022-12-12 12:35:29 +0100

    created IncrementCounter component and added it to ProductMainView

--- addons/point_of_sale/static/src/self_order/ProductMainView/ProductMainView.js
+++ addons/point_of_sale/static/src/self_order/ProductMainView/ProductMainView.js
@@ -1,13 +1,12 @@
 /** @odoo-module */
 
-const { Component, mount, xml, onRendered, whenReady, App} = owl;
+const { Component, useState} = owl;
 import { _t } from "@web/core/l10n/translation";
 import { NavBar } from '../NavBar/NavBar.js';
+import { IncrementCounter } from '../UtilComponents/IncrementCounter/IncrementCounter.js';
 export class ProductMainView extends Component {
     setup() {
-        onRendered(() => {
-                console.log('Rendered:', this.constructor.name);
-        });
+        this.state = useState({ quantity : 1  });
     }
     product =       {
             name: "Coca-Cola",
@@ -15,7 +14,12 @@ export class ProductMainView extends Component {
             description: "Coca-Cola is a carbonated soft drink manufactured by The Coca-Cola Company. Originally intended as a patent medicine, it was invented in the late 19th century by John Stith Pemberton and was bought out by businessman Asa Griggs Candler, whose marketing tactics led Coca-Cola to its dominance of the world soft-drink market throughout the 20th century.",
             image: "https://www.coca-cola.be/content/dam/journey/be/nl/private/brands/coca-cola/coca-cola.png",
         }
-    static components = { NavBar };  
+    setQuantity = (quantity) => {
+        if(this.state.quantity + quantity >= 0){
+            this.state.quantity = quantity;
+        }
+    }
+    static components = { NavBar, IncrementCounter };  
 }
 ProductMainView.template = 'ProductMainView'
 export default { ProductMainView };
