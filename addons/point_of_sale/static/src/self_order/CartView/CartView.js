91d81813c6deec010678785dabfb7a6dac87cb28
vlst <vlst@odoo.com>
2022-12-12 21:21:34 +0100

    m

--- addons/point_of_sale/static/src/self_order/CartView/CartView.js
+++ addons/point_of_sale/static/src/self_order/CartView/CartView.js
@@ -2,10 +2,11 @@
 
 const { Component } = owl;
 import { _t } from "@web/core/l10n/translation";
-
+import { NavBar } from "../NavBar/NavBar.js";
 export class CartView extends Component {
     setup() {
     }
+    static components = { NavBar }; 
 }
 CartView.template = 'CartView'
 export default { CartView };
