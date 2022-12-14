ddf9b272a309a0786b0dab012d3d0e3118d9e9ad
vlst <vlst@odoo.com>
2022-12-14 10:12:18 +0100

    products appear in the frontend - still have to figure out how to get the images

--- addons/point_of_sale/static/src/self_order/ProductMainView/ProductMainView.js
+++ addons/point_of_sale/static/src/self_order/ProductMainView/ProductMainView.js
@@ -8,12 +8,6 @@ export class ProductMainView extends Component {
     setup() {
         this.state = useState({ quantity : 1  });
     }
-    product =       {
-            name: "Coca-Cola",
-            price: 2.5,
-            description: "Coca-Cola is a carbonated soft drink manufactured by The Coca-Cola Company. Originally intended as a patent medicine, it was invented in the late 19th century by John Stith Pemberton and was bought out by businessman Asa Griggs Candler, whose marketing tactics led Coca-Cola to its dominance of the world soft-drink market throughout the 20th century.",
-            image: "https://www.coca-cola.be/content/dam/journey/be/nl/private/brands/coca-cola/coca-cola.png",
-        }
     setQuantity = (quantity) => {
         if(this.state.quantity + quantity >= 0){
             this.state.quantity = quantity;
--- addons/point_of_sale/static/src/self_order/SelfOrderRoot.js
+++ addons/point_of_sale/static/src/self_order/SelfOrderRoot.js
@@ -24,106 +24,23 @@ class SelfOrderRoot extends Component {
         this.menu = "";
         // this function makes a request to the server to get the menu
         onWillStart(async () => {
-            this.menu = await rpc('/pos/self-order/get-menu');
+            this.productList = await rpc('/pos/self-order/get-menu');
         }); 
-
     }
     
-    productList = [
-        {
-            id: 0,
-            name: "Coca-Cola",
-            price: 2.5,
-            description: "Coca-Cola is a carbonated soft drink manufactured by The Coca-Cola Company. Originally intended as a patent medicine, it was invented in the late 19th century by John Stith Pemberton and was bought out by businessman Asa Griggs Candler, whose marketing tactics led Coca-Cola to its dominance of the world soft-drink market throughout the 20th century.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg",
+    // productList = [
+    //     {
+    //         id: 0,
+    //         name: "Coca-Cola",
+    //         price: 2.5,
+    //         description: "Coca-Cola is a carbonated soft drink manufactured by The Coca-Cola Company. Originally intended as a patent medicine, it was invented in the late 19th century by John Stith Pemberton and was bought out by businessman Asa Griggs Candler, whose marketing tactics led Coca-Cola to its dominance of the world soft-drink market throughout the 20th century.",
+    //         image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg",
 
-        },
-        {
-            id: 1,
-            name: "Fanta",
-            price: 2.5,
-            description: "Fanta is a brand of fruit-flavored carbonated soft drinks created by Coca-Cola in 1940. The original Fanta flavors were orange, lemon, and grape. In 1944, pineapple and lime were added to the line-up.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg",
-        },
-        {
-            id: 2,
-            name: "Sprite",
-            price: 2.5,
-            description: "Sprite is a colorless, caffeine-free, lemon and lime-flavored soft drink created by The Coca-Cola Company. It was first developed in West Germany in 1959 as Fanta Klare Zitrone by the Coca-Cola owned Glas Cola company. Sprite was introduced in the United States in 1961 as a competitor to 7 Up. In 1963, the name was changed to Sprite, and the flavor was reformulated to be less sweet and more lemon-lime flavored. Sprite was introduced in the United Kingdom in 1964, and in Canada in 1965. Sprite is now available in more than 190 countries.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg",
-        },
-        {   
-            id: 3,
-            name: "Coca-Cola Zero",
-            price: 2.5,
-            description: "Coca-Cola Zero Sugar is a sugar-free cola-flavored soft drink produced by The Coca-Cola Company. It is a variant of Coca-Cola, with an intense sweetener blend of aspartame, acesulfame potassium, and sucralose. Coca-Cola Zero Sugar was introduced in 2006 in the United States, and in 2007 in the United Kingdom.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg",
-        },
-        {   
-            id: 4,
-            name: "Burger",
-            price: 5,
-            description: "A hamburger, beefburger, burger or hamburg is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun. The patty may be pan fried, barbecued, or flame broiled. Hamburgers are often served with cheese, lettuce, tomato, onion, pickles, bacon, or chiles.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg", 
-        },
-        {
-            id: 5,
-            name: "Fries",
-            price: 3,
-            description: "French fries, or simply fries, chips, finger chips, or French-fried potatoes, are batonnet or allumette-cut deep-fried potatoes. The potatoes are peeled and then sliced into uniform sticks about 12 to 25 mm (0.5 to 1 in) thick, depending on the variety and size of potato. They are then deep-fried in oil at 180 to 190 °C (356 to 374 °F) until golden brown.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg",
-        },
-        {
-            id: 6,
-            name: "Chicken Nuggets",
-            price: 4,
-            description: "Chicken nuggets are chicken pieces made from chicken breast fillets or chicken thigh meat, formed into a small patty, breaded, and deep-fried. Chicken nuggets are a popular fast food item in the United States, Canada, the United Kingdom, Australia, New Zealand, and other countries.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg",
-        },
-        {
-            id: 7,
-            name: "Chicken Burger",
-            price: 5,
-            description: "A hamburger, beefburger, burger or hamburg is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun. The patty may be pan fried, barbecued, or flame broiled. Hamburgers are often served with cheese, lettuce, tomato, onion, pickles, bacon, or chiles.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg", 
-        },
-        {
-            id: 8,
-            name: "Chicken Fries",
-            price: 3,
-            description: "French fries, or simply fries, chips, finger chips, or French-fried potatoes, are batonnet or allumette-cut deep-fried potatoes. The potatoes are peeled and then sliced into uniform sticks about 12 to 25 mm (0.5 to 1 in) thick, depending on the variety and size of potato. They are then deep-fried in oil at 180 to 190 °C (356 to 374 °F) until golden brown.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg",
-        },
-        {
-            id: 9,
-            name: "Chicken Nuggets",
-            price: 4,
-            description: "Chicken nuggets are chicken pieces made from chicken breast fillets or chicken thigh meat, formed into a small patty, breaded, and deep-fried. Chicken nuggets are a popular fast food item in the United States, Canada, the United Kingdom, Australia, New Zealand, and other countries.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg",
-        },
-        {
-            id: 10,
-            name: "Burger",
-            price: 5,
-            description: "A hamburger, beefburger, burger or hamburg is a sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun. The patty may be pan fried, barbecued, or flame broiled. Hamburgers are often served with cheese, lettuce, tomato, onion, pickles, bacon, or chiles.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg", 
-        },
-        {
-            id: 11,
-            name: "Fries",
-            price: 3,
-            description: "French fries, or simply fries, chips, finger chips, or French-fried potatoes, are batonnet or allumette-cut deep-fried potatoes. The potatoes are peeled and then sliced into uniform sticks about 12 to 25 mm (0.5 to 1 in) thick, depending on the variety and size of potato. They are then deep-fried in oil at 180 to 190 °C (356 to 374 °F) until golden brown.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg",
-        },
-        {
-            id: 12,
-            name: "Chicken Nuggets",
-            price: 4,
-            description: "Chicken nuggets are chicken pieces made from chicken breast fillets or chicken thigh meat, formed into a small patty, breaded, and deep-fried. Chicken nuggets are a popular fast food item in the United States, Canada, the United Kingdom, Australia, New Zealand, and other countries.",
-            image: "https://images2.minutemediacdn.com/image/fetch/w_2000,h_2000,c_fit/https%3A%2F%2Ffoodsided.com%2Ffiles%2F2021%2F05%2FWendys-Daves-Triple.jpg",
-        }
-    ];
-
+    //     },
+    // ];
+    jsonToString = (json) => {
+        return JSON.stringify(json);
+    }
 
     //TODO: Add a method to get the table number from the response of the server
     tableNumber = odoo.table_number;
@@ -157,7 +74,7 @@ class SelfOrderRoot extends Component {
     }
     getTotalCartCost = () => {
         return this.state.cart.reduce((sum, cartItem) => {
-            return sum +  this.productList[cartItem.id].price * cartItem.quantity;
+            return sum +  this.productList[cartItem.id].list_price * cartItem.quantity;
         },0);
     }
     // TODO: Find the currency type of the posConfig
