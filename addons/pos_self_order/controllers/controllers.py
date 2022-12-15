0828829978400c65244b4035f11868e67155166e
vlst <vlst@odoo.com>
2022-12-15 16:42:22 +0100

    changed route for getting images

--- addons/pos_self_order/controllers/controllers.py
+++ addons/pos_self_order/controllers/controllers.py
@@ -9,8 +9,10 @@ from odoo.addons.account.controllers.portal import PortalAccount
 
 
 class PosSelfOrder(http.Controller):
-    # this is the main controller for the POS Self Order App
+    # this is the controller for the POS Self Order App
     # The user gets this route from the QR code that they scan at the table
+    # This START ROUTE will render the LANDIN PAGE of the POS Self Order App
+    # And it will pass some generic variabiles to the template: pos_id, table_number, pos_name, currency
     @http.route('/pos-self-order/start/<int:pos_id>/<int:table_number>/', auth='public', website=True)
     def pos_self_order_start(self, table_number, pos_id):
         # We know the id of the POS from the QR code
@@ -33,7 +35,6 @@ class PosSelfOrder(http.Controller):
     # this is the route that the POS Self Order App uses to GET THE MENU
     @http.route('/pos-self-order/get-menu', auth='public',type="json", website=True)
     def pos_self_order_get_menu(self,config_id = False):
-        # response.headers['Cache-Control'] = 'no-store'
         # Let me break this down:
         # 0. We request the product.product model
         # 1. We are using the sudo() method to bypass the access rights
@@ -42,14 +43,6 @@ class PosSelfOrder(http.Controller):
         # 4. We are using the type="json" argument in the function call to return the response as JSON
         response = http.request.env['product.product'].sudo().search([('available_in_pos', '=', True)]).read(['id', 'name', 'list_price', 'description_sale'])
         return response
-    @http.route('/pos-self-order/get-comp/<int:pos_id>', auth='public', type="json", website=True)
-    def pos_self_order_get_comp(self,pos_id):
-        # pos_details = http.request.env['pos.config'].sudo().browse(pos_id).read([])[0]
-        pos_details = http.request.env['pos.config'].sudo().search_read([('id', '=', pos_id)], [])[0]
-        # company_name = http.request.env['res.company'].sudo().search_read([('id' '=', pos_details.get('company_id')[0])], ['name'])[0]
-        # import pdb; pdb.set_trace()
-
-        return pos_details
     # this is the route that the POS Self Order App uses to GET THE PRODUCT IMAGES
     @http.route('/pos-self-order/get-images/<int:product_id>', methods=['GET'], type='http', auth='public')
     def pos_self_order_get_images(self, product_id, **kwargs):
@@ -59,3 +52,25 @@ class PosSelfOrder(http.Controller):
         # 'image_1920' is the name of the field that contains the image
         # If the product does not have an image, the function _get_image_stream_from will return the default image
         return request.env['ir.binary']._get_image_stream_from(product, field_name='image_1920').get_response()
+    @http.route('/pos-self-order/send-order', auth='public', type="json", website=True)
+    def pos_self_order_send_order(self):
+        print("send order")
+        print("send order")
+        print("send order")
+        print("send order")
+        print("send order")
+        print("send order")
+        print("send order")
+        print("send order")
+        print("send order")
+        print("send order")
+        print("send order")
+        # print the request data
+        print(request.jsonrequest)
+        print(request)
+        print("send order")
+        print("send order")
+        print("send order")
+        print("send order")
+        print("send order")
+        return 
