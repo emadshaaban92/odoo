16d8cb918dbbbb353601498992a986ab6952b7c2
Julien Van Roy <juvr@odoo.com>
2022-12-16 12:02:35 +0100

    [IMP] l10n_mx_edi_website_sale: add cfdi_to_public field
    Allow to send a CFDI as "publico en general" by adding a field on the account.move and
    on the sale.order. Also add the field `l10n_mx_edi_usage` on the SO.
    When creating a move from a SO, the value of these fields are copied.
    
    In addition, add an extra tab "Invoicing Info" on the ecommerce for the customer to indicate
    whether he wants an invoice or not. If he wants an invoice, ask for the fiscal regime,
    the usage and the tax breakdown. If he doesn't want an invoice, it means we can send the
    related invoice in "publico en general" mode.
    
    The extra info tab appears only if:
    * the company is mexican
    * the automatic_invoice setting is True
    
    When displaying the extra info tab, we also need to add a flag to all the templates rendered
    during the checkout flow (because each of them calls the wizard_checkout template, responsible
    for the rendering of a continuous line on top of the screen with the current step of the checkout we
    are at), these template's ids are:
    * cart
    * address
    * checkout
    * extra_info
    * payment
    
    task-3089169

--- addons/website_sale/controllers/main.py
+++ addons/website_sale/controllers/main.py
@@ -732,6 +732,9 @@ class WebsiteSale(http.Controller):
                 order_sudo._cart_update_pricelist(update_pricelist=True)
         return request.redirect(redirect)
 
+    def _cart_values(self, **post):
+        return {}
+
     @http.route(['/shop/cart'], type='http', auth="public", website=True, sitemap=False)
     def cart(self, access_token=None, revive='', **post):
         """
@@ -776,6 +779,7 @@ class WebsiteSale(http.Controller):
             # force no-cache so IE11 doesn't cache this XHR
             return request.render("website_sale.cart_popover", values, headers={'Cache-Control': 'no-cache'})
 
+        values.update(self._cart_values(**post))
         return request.render("website_sale.cart", values)
 
     @http.route(['/shop/cart/update'], type='http', auth="public", methods=['POST'], website=True)
@@ -1382,6 +1386,9 @@ class WebsiteSale(http.Controller):
     # ------------------------------------------------------
     # Extra step
     # ------------------------------------------------------
+    def _extra_info_values(self, **post):
+        return {}
+
     @http.route(['/shop/extra_info'], type='http', auth="public", website=True, sitemap=False)
     def extra_info(self, **post):
         # Check that this option is activated
@@ -1402,6 +1409,7 @@ class WebsiteSale(http.Controller):
             'partner': order.partner_id.id,
             'order': order,
         }
+        values.update(self._extra_info_values(**post))
         return request.render("website_sale.extra_info", values)
 
     # ------------------------------------------------------
