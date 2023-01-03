6d46e5b5708a8b643f055295310972698881b9fe
Julien Castiaux <juc@odoo.com>
2023-01-03 19:19:32 +0100

    [FIX] base: order imports in ir_actions_report

--- odoo/addons/base/models/ir_actions_report.py
+++ odoo/addons/base/models/ir_actions_report.py
@@ -1,6 +1,23 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+import io
+import json
+import logging
+import lxml.html
+import os
+import re
+import subprocess
+import tempfile
+
+from collections import OrderedDict
+from collections.abc import Iterable
+from contextlib import closing
+
+from lxml import etree
 from markupsafe import Markup
+from PIL import Image, ImageFile
+from PyPDF2 import PdfFileWriter, PdfFileReader, utils
+from reportlab.graphics.barcode import createBarcodeDrawing
 
 from odoo import api, fields, models, tools, SUPERUSER_ID, _
 from odoo.exceptions import UserError, AccessError
@@ -10,22 +27,7 @@ from odoo.tools import check_barcode_encoding, config, is_html_empty, parse_vers
 from odoo.http import request
 from odoo.osv.expression import NEGATIVE_TERM_OPERATORS, FALSE_DOMAIN
 
-import io
-import logging
-import os
-import lxml.html
-import tempfile
-import subprocess
-import re
-import json
 
-from lxml import etree
-from contextlib import closing
-from reportlab.graphics.barcode import createBarcodeDrawing
-from PyPDF2 import PdfFileWriter, PdfFileReader, utils
-from collections import OrderedDict
-from collections.abc import Iterable
-from PIL import Image, ImageFile
 # Allow truncated images
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
