26782fcdd4b7980a801c151017fa5e98d25e6dd4
Noe Antoine <nan@odoo.com>
2023-01-05 17:10:24 +0100

    [WIP] adapt res.partner fields to the new member_status
    We consider that invited members should not be a part of these fields,
    in order to keep their original meaning
    
    To confirm with LNA
    - in the counter of res.partner stat button
    - in the view from stat button
    
    CHANGE (upg?)
    M2M is now a compute + search, before it used a relative table!

--- addons/website_slides/models/res_partner.py
+++ addons/website_slides/models/res_partner.py
@@ -2,14 +2,17 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, _
+from odoo.osv import expression
 
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
 
     slide_channel_ids = fields.Many2many(
-        'slide.channel', 'slide_channel_partner', 'partner_id', 'channel_id',
-        string='eLearning Courses', groups="website_slides.group_website_slides_officer")
+        'slide.channel', string='eLearning Courses',
+        compute='_compute_slide_channel_ids',
+        search='_search_slide_channel_ids',
+        groups="website_slides.group_website_slides_officer")
     slide_channel_completed_ids = fields.One2many(
         'slide.channel', string='Completed Courses',
         compute='_compute_slide_channel_completed_ids',
@@ -21,6 +24,21 @@ class ResPartner(models.Model):
     slide_channel_company_count = fields.Integer(
         'Company Course Count', compute='_compute_slide_channel_company_count',
         groups="website_slides.group_website_slides_officer")
+
+    def _compute_slide_channel_ids(self):
+        slide_channel_partners = self.env['slide.channel.partner'].search([
+            ('partner_id', 'in', self.ids),
+            ('member_status', '!=', 'invited')
+        ])
+        for partner in self:
+            partner.slide_channel_ids = slide_channel_partners.filtered(lambda scp: scp.partner_id == partner).mapped('channel_id')
+
+    def _search_slide_channel_ids(self, operator, value):
+        cp_enrolled = self.env['slide.channel.partner'].sudo().search([
+            ('channel_id', operator, value),
+            ('member_status', '!=', 'invited')
+        ])
+        return [('id', 'in', cp_enrolled.partner_id.ids)]
 
     def _compute_slide_channel_completed_ids(self):
         for partner in self:
@@ -59,13 +77,15 @@ class ResPartner(models.Model):
     def action_view_courses(self):
         """ View partners courses. In singleton mode, return courses followed
         by all its contacts (if company) or by themselves (if not a company).
-        Otherwise simply set a domain on required partners. """
+        Otherwise simply set a domain on required partners. The courses to which
+        the partner(s) is not enrolled (e.g. invited) are not shown. """
         action = self.env["ir.actions.actions"]._for_xml_id("website_slides.slide_channel_partner_action")
-        action['name'] = _('Followed Courses')
+        action['display_name'] = _('Followed Courses')
+        action['domain'] = [('member_status', '!=', 'invited')]
         if len(self) == 1 and self.is_company:
-            action['domain'] = [('partner_id', 'in', self.child_ids.ids)]
+            action['domain'] = expression.AND([action['domain'], [('partner_id', 'in', self.child_ids.ids)]])
         elif len(self) == 1:
             action['context'] = {'search_default_partner_id': self.id}
         else:
-            action['domain'] = [('partner_id', 'in', self.ids)]
+            action['domain'] = expression.AND([action['domain'], [('partner_id', 'in', self.ids)]])
         return action
