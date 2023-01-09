eb768f18efa29d315fb05f4cd8e27a55a74fcc76
Rahul Prajapati <rapr@odoo.com>
2023-01-09 15:45:07 +0530

    Patch Component Extend Registries.Component.extend

--- addons/l10n_fr_pos_cert/static/src/js/Chrome.js
+++ addons/l10n_fr_pos_cert/static/src/js/Chrome.js
@@ -1,24 +1,19 @@
 /** @odoo-module */
 
 import Chrome from "@point_of_sale/js/Chrome";
-import Registries from "@point_of_sale/js/Registries";
+import { patch } from "@web/core/utils/patch";
 
-const PosFrCertChrome = (Chrome) =>
-    class extends Chrome {
-        async start() {
-            await super.start();
-            if (this.env.pos.is_french_country() && this.env.pos.pos_session.start_at) {
-                const now = Date.now();
-                const limitDate = new Date(this.env.pos.pos_session.start_at);
-                limitDate.setDate(limitDate.getDate() + 1);
-                if (limitDate.getTime() < now) {
-                    const info = await this.env.pos.getClosePosInfo();
-                    this.showPopup("ClosePosPopup", { info: info });
-                }
+patch(Chrome.prototype, "pos_france_certificate.Chrome", {
+    async start() {
+        await this._super();
+        if (this.env.pos.is_french_country() && this.env.pos.pos_session.start_at) {
+            const now = Date.now();
+            const limitDate = new Date(this.env.pos.pos_session.start_at);
+            limitDate.setDate(limitDate.getDate() + 1);
+            if (limitDate.getTime() < now) {
+                const info = await this.env.pos.getClosePosInfo();
+                this.showPopup("ClosePosPopup", { info: info });
             }
         }
-    };
-
-Registries.Component.extend(Chrome, PosFrCertChrome);
-
-export default Chrome;
+    },
+});
